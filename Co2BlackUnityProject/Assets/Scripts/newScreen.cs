e3c9c244de7a56700da4e8de9cb9abc5eb935bc7
Erin Michelle <emw337@drexel.edu>
2017-11-25 22:09:50 -0500

    smoke and map

---
+++ Co2BlackUnityProject/Assets/Scripts/newScreen.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class newScreen : MonoBehaviour {
+
+	public GameObject Screen3;
+	public GameObject Screen2;
+
+	public void visualize(){
+		Screen3.SetActive (true);
+		Screen2.SetActive (false);
+	}
+}
