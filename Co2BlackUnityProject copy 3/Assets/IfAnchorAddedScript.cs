acecd31ea75471ec84373be43d878c94cfd8946f
Martina Calluori <mpc94@drexel.edu>
2018-01-15 18:55:49 -0500

    fixed point at floor issue

---
+++ Co2BlackUnityProject copy 3/Assets/IfAnchorAddedScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.iOS;
+
+public class IfAnchorAddedScript : MonoBehaviour {
+
+	private UnityARAnchorManager unityARAnchorManager;
+	public GameObject FindFlatSurfaceIcon;
+	public GameObject FindFlatSurfaceText;
+	public GameObject TapToPlaceText;
+	public GameObject ObjectFocusSquare;
+	public Material lampShade;
+
+
+	// Use this for initialization
+	void Start () {
+		unityARAnchorManager = new UnityARAnchorManager();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		List<ARPlaneAnchorGameObject> arpags = unityARAnchorManager.GetCurrentPlaneAnchors ();
+		if(arpags.Count > 0) {
+			Debug.Log ("Plane Added");
+			FindFlatSurfaceIcon.SetActive (false);
+			FindFlatSurfaceText.SetActive (false);
+			TapToPlaceText.SetActive (true);
+			ObjectFocusSquare.SetActive (true);
+		}
+
+	}
+}
