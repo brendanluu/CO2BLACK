e0211170facb63a37f4e7a95b5854b39b194edba
BrendanRusso <BrendanRusso@n3-55-171.dhcp.drexel.edu>
2018-01-22 22:59:11 -0500

    begin trash

---
+++ Co2BlackUnityProject/Assets/ignoreCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ignoreCollision : MonoBehaviour {
+	public GameObject theobjectToNotIgnore;
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == "badTrash") {
+			Physics.IgnoreCollision (theobjectToNotIgnore.GetComponent<Collider>(), GetComponent<Collider>());
+		}
+	}
+
+}
