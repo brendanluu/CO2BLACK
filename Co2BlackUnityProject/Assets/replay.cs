9046fbde75e72b753429e1f41e88b555bfa1edd1
Martina Calluori <mpc94@drexel.edu>
2018-01-24 10:49:56 -0500

    meta

--- Co2BlackUnityProject/Assets/replay.cs
+++ Co2BlackUnityProject/Assets/replay.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class replay : MonoBehaviour {
 
 
-	public void ModeSelect(){
-		StartCoroutine ("Wait");
-		Initiate.Fade("WiegmanRussoContrast",Color.black,1.0f);
-	}
-
-	IEnumerator Wait(){
-		yield return new WaitForSeconds (1.3f);
-		Application.LoadLevel ("WiegmanRussoContrast");
-	}
-
+//	public void ModeSelect(){
+//		StartCoroutine ("Wait");
+//		Initiate.Fade("WiegmanRussoContrast",Color.black,1.0f);
+//	}
+//
+//	IEnumerator Wait(){
+//		yield return new WaitForSeconds (1.3f);
+//		Application.LoadLevel ("WiegmanRussoContrast");
+//	}
+//
 
 }
 
