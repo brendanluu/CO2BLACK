e416cd6e724c639d3ccb6a5ae7a5e31e71022309
Erin Michelle <emw337@drexel.edu>
2018-02-04 16:07:04 -0500

    started third module

---
+++ Co2BlackUnityProject/Assets/Scripts/EnergyHit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyHit : MonoBehaviour {
+
+	//first screen 
+	public GameObject coal;
+	public GameObject solar;
+	public GameObject chooseEnergy;
+
+	//second screen 
+	public GameObject energyFact;
+	public GameObject pointer;
+
+	public Material smokeHigh;
+
+	void HitByRay () {
+		//first screen
+		Debug.Log ("I was hit by a Ray");
+
+		coal.SetActive (false);
+		//trigger animation of solar pamphlet getting larger and centered
+		chooseEnergy.SetActive (false);
+		pointer.SetActive (true);
+		energyFact.SetActive (true);
+
+		smokeHigh.SetFloat ("_Alpha", 0.15f);
+
+	}
+
+}
