524843ede86ea19846a82b50844cf9c587f69924
Martina Calluori <mpc94@drexel.edu>
2018-02-17 18:14:15 -0500

    added third

---
+++ Co2BlackUnityProject/Assets/Scripts/thirdmod.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thirdmod : MonoBehaviour {
+
+	public GameObject fire;
+	public GameObject coal;
+	public GameObject gas;
+	public GameObject clean;
+
+
+	void turnOnCoal() {
+		coal.SetActive (true);
+	}
+
+	void turnOnGas(){
+		gas.SetActive (true);
+	}
+
+	void turnOnClean(){
+		clean.SetActive (true);
+	}
+
+
+}
