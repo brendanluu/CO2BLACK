ba34d2123258e0510b18927ea626ab68c9a62f67
Jake Culp <jakeculp@gmail.com>
2018-02-18 19:00:00 -0500

    Implemented two different versions of tap anywhere to lamp scene

--- Co2BlackUnityProject/Assets/Scripts/lampBtns.cs
+++ Co2BlackUnityProject/Assets/Scripts/lampBtns.cs
@@ -9,7 +9,9 @@ public class lampBtns : MonoBehaviour {
 	public GameObject BuildingFactBot_Trans;
 	public GameObject BuildingFactBot;
 	public GameObject dontForget;
+	public GameObject dontForgetTrans;
 	public GameObject turnOff;
+	public GameObject turnOffTrans;
 	public GameObject pointer;
 
 
@@ -24,13 +26,16 @@ public class lampBtns : MonoBehaviour {
 	public void BuildingFactBotBtn () {
 
 		BuildingFactBot.SetActive (false);
+		BuildingFactBot_Trans.SetActive (false);
+		dontForgetTrans.SetActive (true);
 		dontForget.SetActive (true);
 	}
 
 	public void dontForgetBtn () {
-
+		dontForgetTrans.SetActive (false);
 		dontForget.SetActive (false);
 		pointer.SetActive (false);
+		turnOffTrans.SetActive (true);
 		turnOff.SetActive (true);
 	}
 
